77506f4e23fc3b8790dee1222653df93c32163f4
Tarakshgoyal <taraksh9a33@gmail.com.com>
2025-03-29 12:52:09 +0530

    acm

--- src/components/SparklesPreview.tsx
+++ src/components/SparklesPreview.tsx
@@ -1,10 +1,16 @@
 "use client";
 import React from "react";
 import { SparklesCore } from "@/components/ui/sparkles";
+import Image from "next/image";
 
 export function SparklesPreview() {
   return (
     <div className="h-[44rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="flex items-center justify-between overflow-hidden ml-10 rounded-md absolute top-0 left-0 right-0 z-10">
+        <Image src={"/acmlogoo.png"} width={100} height={100} alt={"logo"}/>
+        <Image src={"/ACM.png"} width={180} height={100} alt={"logo"}/>
+        
+      </div>
       <div className="w-full absolute inset-0 h-screen">
         <SparklesCore
           id="tsparticlesfullpage"
